6a63652d665ad454ab068a7827757a231529a78e
André Sarandeses <acamposarandeses@gmail.com>
2024-10-25 00:56:06 +0100

    Moved load_network

--- BioInfoToolkit/RuleBasedModel/network/reaction_network.py
+++ BioInfoToolkit/RuleBasedModel/network/reaction_network.py
@@ -379,3 +379,50 @@ class ReactionNetwork:
         with open(fp, 'w', encoding='utf-8') as net_file:
             out = self.as_string()
             net_file.write(out)
+
+
+def load_network(file_path: str) -> ReactionNetwork:
+    network = ReactionNetwork()
+
+    current_block = None
+    with open(file_path, 'r', encoding="utf-8") as file:
+        for line in file:
+            line = line.strip()
+
+            # Skip empty lines
+            if not line:
+                continue
+
+            # Detect the beginning of a block
+            if line.startswith('begin'):
+                current_block = line.split()[1]
+                continue
+
+            # Detect the end of a block
+            if line.startswith('end'):
+                current_block = None
+                continue
+
+            # Parse lines depending on the current block
+            if current_block == 'parameters':
+                parsed_parameter = parse_parameters(line)
+                parameter = Parameter.from_dict(parsed_parameter)
+                network.parameters_block.add_parameter(parameter)
+
+            elif current_block == 'species':
+                parsed_species = parse_seed_species(line)
+                species = Species.from_dict(parsed_species, None)
+                network.species_block.add_specie(species)
+
+            elif current_block == 'reactions':
+                reaction = Reaction.from_declaration(line)
+                network.reactions_block.add_reaction(reaction)
+
+            elif current_block == 'groups':
+                group = ObservablesGroup.from_declaration(line)
+                network.groups_block.add_group(group)
+
+    # set filepaths for output files
+    network.set_output_filepaths(file_path)
+
+    return network
