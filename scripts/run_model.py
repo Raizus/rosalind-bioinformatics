4b1409c616d75336dae7c4a7a95302f01fd179ab
André Sarandeses <acamposarandeses@gmail.com>
2024-10-29 23:30:12 +0000

    Updated run script

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -6,8 +6,14 @@ from BioInfoToolkit.RuleBasedModel.utils.utls import read_simulation_data
 
 
 if __name__ == "__main__":
-    fp = "./BioInfoToolkit/RuleBasedModel/assets/chemotaxis1.bngl"
+    # fp = "./BioInfoToolkit/RuleBasedModel/assets/chemotaxis3.bngl"
+    # fp = "./BioInfoToolkit/RuleBasedModel/assets/chemotaxis2.bngl"
+    # fp = "./BioInfoToolkit/RuleBasedModel/assets/chemotaxis1.bngl"
+    # fp = "./BioInfoToolkit/RuleBasedModel/assets/repressilator.bngl"
+    fp = "./BioInfoToolkit/RuleBasedModel/assets/FceRI_ji.bngl"
+    # fp = "./BioInfoToolkit/RuleBasedModel/assets/egfr_simple.bngl"
     model, actions = load_bngl(fp)
+    print(model.as_string())
     network = apply_actions(model, actions)
     if network:
         gdat = network.gdat_filename
